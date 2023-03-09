723b047cfa442b69c0aa7b0c276d294fea0fc0a6
LifelikeDev <primeguyy@gmail.com>
2023-03-09 10:31:55 +0000

    basic note tracker: add & delete note

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [note, setNote] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleOnChangeText = (enteredText) => {
+    setNote(enteredText);
+  };
+
+  const handleOnPressButton = () => {
+    setList((prevNotes) => [...prevNotes, note]);
+    setNote("");
+  };
+
+  const handleItemDelete = (selectedItemIndex) => {
+    const filtered = list.filter((_, id) => id !== selectedItemIndex);
+    setList(filtered);
+  };
+
+  console.log(list);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text
+        style={{
+          fontSize: 20,
+          margin: 30,
+          fontWeight: 500,
+          fontStyle: "italic",
+        }}
+      >
+        Hello React Native
+      </Text>
+
+      {/* Input Form */}
+
+      <View style={{ width: 300 }}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Add Your Note Here"
+            style={styles.input}
+            value={note}
+            onChangeText={handleOnChangeText}
+          />
+
+          <Button onPress={handleOnPressButton} title="Add Note" />
+        </View>
+
+        {/* List */}
+
+        <View style={styles.listContainer}>
+          {list.map((el, idx) => (
+            <Text
+              key={`note: ${idx}`}
+              style={styles.listItem}
+              onPress={() => handleItemDelete(idx)}
+            >
+              {el}
+            </Text>
+          ))}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  nestedContainer: {
+    backgroundColor: "#bbb",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 100,
+    width: 200,
+  },
+  inputContainer: {
+    padding: 10,
+    paddingBottom: 30,
+    flexDirection: "row",
+    gap: 2,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    flex: 1,
+  },
+  listContainer: {
+    paddingTop: 30,
+  },
+  listItem: {
+    borderRadius: 1,
+    borderColor: "red",
+    backgroundColor: "#282872",
+    padding: 10,
+    marginBottom: 20,
+    color: "white",
+    fontSize: 20,
   },
 });
