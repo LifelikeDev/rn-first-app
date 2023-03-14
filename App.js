22bfe65290d25a2007455f0db56180f4c0512028
LifelikeDev <primeguyy@gmail.com>
2023-03-14 22:49:58 +0000

    import screens with react navigation

--- App.js
+++ App.js
@@ -3,14 +3,22 @@ import { StyleSheet, Text, View } from "react-native";
 import ProductListing from "./screens/productListing";
 import Favourites from "./screens/favourites";
 import ProductDetails from "./screens/productDetails";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductListing />
-      <Favourites />
-      <ProductDetails />
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="productListing" component={ProductListing} />
+          <Stack.Screen name="favourites" component={Favourites} />
+          <Stack.Screen name="productDetails" component={ProductDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -19,7 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
