d4ab47abc9dfee7bb037bccdf61e1b03b420039f
LifelikeDev <primeguyy@gmail.com>
2023-03-15 21:56:21 +0000

    fetch products from api

--- context/index.js
+++ context/index.js
@@ -3,12 +3,26 @@
 //  ii. provide the context
 // iii. consume that context
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const Context = createContext(null);
 
 const ProductContext = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  // api call
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+
+      if (data) setProducts(data.products);
+    };
+
+    fetchProducts();
+  }, []);
+
+  console.log({ products });
 
   return <Context.Provider value={{ products }}>{children}</Context.Provider>;
 };
