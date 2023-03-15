c70cbb1580a3388caab4b3c7b4d38a740de2a0be
LifelikeDev <primeguyy@gmail.com>
2023-03-15 22:45:12 +0000

    display fetched products

--- context/index.js
+++ context/index.js
@@ -5,26 +5,34 @@
 
 import { createContext, useEffect, useState } from "react";
 
-const Context = createContext(null);
+export const Context = createContext(null);
 
 const ProductContext = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // api call
   useEffect(() => {
+    setLoading(true);
+
     const fetchProducts = async () => {
       const response = await fetch("https://dummyjson.com/products");
       const data = await response.json();
 
-      if (data) setProducts(data.products);
+      if (data) {
+        setLoading(false);
+        setProducts(data.products);
+      }
     };
 
     fetchProducts();
   }, []);
 
-  console.log({ products });
-
-  return <Context.Provider value={{ products }}>{children}</Context.Provider>;
+  return (
+    <Context.Provider value={{ products, loading }}>
+      {children}
+    </Context.Provider>
+  );
 };
 
 export default ProductContext;
--- screens/productListing/index.jsx
+++ screens/productListing/index.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { ActivityIndicator } from "react-native";
+import { Context } from "../../context";
 
 export default function ProductListing() {
+  const { loading, products } = useContext(Context);
+
+  if (loading) {
+    return (
+      <ActivityIndicator style={styles.loader} size="large" color="#282872" />
+    );
+  }
+
   return (
     <View>
-      <Text>Product Listing</Text>
+      <FlatList
+        data={products}
+        renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+        keyExtractor={(itemData) => itemData.id}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
