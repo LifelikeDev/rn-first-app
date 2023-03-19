44784551c1af8fe409430576cce03864b5acd0c0
LifelikeDev <primeguyy@gmail.com>
2023-03-19 22:15:21 +0000

    Add favourites modal added

--- screens/productDetails/index.jsx
+++ screens/productDetails/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Modal, Pressable } from "react-native";
 import { ActivityIndicator } from "react-native";
 import ProductDetailsItem from "../../components/productDetails";
 
@@ -12,6 +12,7 @@ export default function ProductDetails() {
 
   const [loading, setLoading] = useState(false);
   const [productDetailsData, setProductDetailsData] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   // handle data fetch
   useEffect(() => {
@@ -36,7 +37,13 @@ export default function ProductDetails() {
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button color="#282872" title="Add Favourites" />;
+        return (
+          <Button
+            onPress={() => setModalVisible(true)}
+            color="#282872"
+            title="Add Favourites"
+          />
+        );
       },
     });
   }, []);
@@ -50,6 +57,28 @@ export default function ProductDetails() {
   return (
     <View>
       <ProductDetailsItem data={productDetailsData} />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -59,5 +88,46 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
