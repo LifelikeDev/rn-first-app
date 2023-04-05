23635a1e60cc18ec57b1eecaf386d229f85243bf
LifelikeDev <primeguyy@gmail.com>
2023-04-05 23:57:06 +0000

    no favourite items view added

--- screens/favourites/index.jsx
+++ screens/favourites/index.jsx
@@ -1,10 +1,34 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Context } from "../../context";
 
 export default function Favourites() {
+  const { favouriteItems } = useContext(Context);
+
+  if (!favouriteItems.length) {
+    return (
+      <View style={styles.noFavourites}>
+        <Text style={styles.noFavouritesText}>No Favourite Items</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>Favourites</Text>
+      {favouriteItems.map((item) => (
+        <Text key={item.id}>{item.title}</Text>
+      ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  noFavourites: {
+    alignItems: "center",
+    padding: 20,
+  },
+  noFavouritesText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
